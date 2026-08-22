cef003bff6e9b14ad457a254892f9ca9a4f1a62a
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-22 17:07:18 +0200

    formaterade

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import ToDoList from './toDoList'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>
-      Hello
-    </h1>
+    <ToDoList>
+      
+    </ToDoList>
   </StrictMode>,
 )
--- src/toDoList.tsx
+++ src/toDoList.tsx
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
 
-
-function ToDoList(){
+function ToDoList() {
     const [list, setList] = useState<string[]>([]);
+    const [input, setInput] = useState<string>("");
 
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
 
+        setList([...list, input]);
+        setInput("");
+    }
 
     return (
         <div>
-            <form id='toDoList'>
+            <form id="toDoList" onSubmit={handleSubmit}>
+
+                {list.map((task) => (
+                    <li>{task}</li>
+                ))}
+
+                <input
+                    type="text"
+                    id="input"
+                    placeholder="Skriv in tasks"
+                    value={input}
+                    onChange={(event) => setInput(event.target.value)}
+                />
+
+                <button type="submit">
+                    Lägg till Task
+                </button>
 
             </form>
-
-            <input type='text' id='input' placeholder='Skriv in tasks'></input>
-            <button type='submit'>Lägg till Task</button>
         </div>
-    )
+    );
 }
+
+export default ToDoList;

6a467015cbf3cf31f0e3e105e80203913b0b43c1
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-22 16:26:14 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -57,3 +57,32 @@ let ourTuple: [number, boolean, string];
 
 ourTuple = [5, true, 'bob'];
 
+const ourReadOnlyTuple: [number, boolean, string] = [5, true, 'the real coding god'];
+
+ourReadOnlyTuple.push('coding god took a day off');
+
+
+const car: {type: string, model: string, year: number} = {
+    type: 'Toyota',
+    model: 'Carolla',
+    year: 2009
+}
+
+
+type Animal = {name: string};
+type Bear = Animal & {honey: boolean};
+const bear: Bear = {name:'Winnie', honey: true};
+
+type Status = "succes" | "error";
+let response: Status = "succes"
+
+
+interface Rectangle {
+    width: number,
+    height: number
+}
+
+const rectangle: Rectangle = {
+    height: 20,
+    width: 30
+};

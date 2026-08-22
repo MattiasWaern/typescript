2acdbd9ff43d165227dd8236639154f4604114ee
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-22 13:02:32 +0200

    c

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function greet(name) {
     return "Hello: ".concat(name);
 }
 var message = greet("World");
 console.log(message);
+var color = "blue";
+var fullName = 'John Doe';
+var age = 30;
+var sentence = "Hello, my namesis ".concat(fullName, " and I'll be ").concat(age + 1, " next year");
+console.log(sentence);
--- test.ts
+++ test.ts
@@ -7,3 +7,13 @@ function greet(name: string): string {
 
 const message: string = greet("World");
 console.log(message);
+
+
+
+
+let color: string = "blue";
+let fullName: string = 'John Doe';
+let age: number = 30;
+let sentence : string = `Hello, my namesis ${fullName} and I'll be ${age + 1} next year`;
+
+console.log(sentence);

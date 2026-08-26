766d5f2b936d42f2b94ba373a1dff1201e007a21
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-26 10:14:27 +0200

    Update practice.tsx

--- src/practice.tsx
+++ src/practice.tsx
@@ -1,2 +1,29 @@
 import React from "react";
 import { useState, useEffect } from "react";
+
+interface Booking{
+    id: number,
+    email: string
+}
+
+let bookings = [];
+
+const randomBooking = (): Booking => {
+    return{
+        id: 123,
+        email: "testtset@gmail.com"
+    }
+}
+
+
+bookings = [randomBooking()];
+
+
+console.log(bookings);
+
+
+function greet(name: string, age: number){
+    return(`Hejsan ${name}, är du ${age} år gammal?`)
+}
+
+console.log(greet('bob', 20))
